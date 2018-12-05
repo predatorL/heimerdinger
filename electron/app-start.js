f4ee75d0e3ce4a87c903975b04d399f359c8fc71
haiyang.li <haiyang.li@asiainnovations.com>
2018-12-05 19:38:57 +0800

    electron：添加app-start；

---
+++ electron/app-start.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+var args = process.argv.splice(2);
+console.log(args);
+
+
+function createWindow() {
+    // 创建浏览器窗体
+    win = new BrowserWindow({width: 800, height: 600});
+    // 加载应用的index.html
+    win.loadFile(path.join(__dirname, './template/zhisiyun.html'));
+}
+
+app.on('ready', createWindow);
