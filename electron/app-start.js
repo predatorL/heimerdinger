b7454a971234ce3efefc298787397815bba5655a
haiyang.li <haiyang.li@asiainnovations.com>
2018-12-06 23:22:05 +0800

    electron: 调整智思云的获取策略;

--- electron/app-start.js
+++ electron/app-start.js
@@ -8,7 +8,7 @@ function createWindow() {
     // 创建浏览器窗体
     win = new BrowserWindow({width: 800, height: 600});
     // 加载应用的index.html
-    win.loadFile(path.join(__dirname, './template/zhisiyun.html'));
+    win.loadFile(path.join(__dirname, './template/index.html'));
 }
 
 app.on('ready', createWindow);
--- electron/app-zhisiyun.js
+++ electron/app-zhisiyun.js
@@ -1,11 +1,14 @@
 const {app, BrowserWindow} = require('electron');
 
+const win = null;
+
 function createWindow() {
     // 创建浏览器窗体
     win = new BrowserWindow({width: 800, height: 600});
 
     // 加载应用的index.html
-    win.loadURL('http://localhost:4200/')
+    win.loadURL('https://www.zhisiyun.com/work_wx_qrcode/')
 }
 
 app.on('ready', createWindow);
+
